5dd9e674dcebb320a6c7f4ea7137cc913749dc4b
TomEder <tomederstal@gmail.com>
2024-10-09 14:53:31 +0200

    Refactor App.js and remove logo.svg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import HeaderComponent from "./components/weatherComponent/Header/HeaderComponent";
+import WeatherComponent from "./components/weatherComponent/WeatherComponent";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <HeaderComponent />
+      <WeatherComponent />
     </div>
   );
 }
